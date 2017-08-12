6ba6e6d427b35f7dac39d49fc8b4d74de23bf455
Henrique Damasceno Vianna <hdvianna@gmail.com>
2017-08-12 13:05:15 -0300

    revisao

--- sw.js
+++ sw.js
@@ -71,15 +71,9 @@ self.addEventListener('fetch', function(e) {
 	console.log('[Service Worker] Fetch', e.request.url);
 	/*
 	 * The app is asking for app shell files. In this scenario the app uses the
-	 * "Cache, falling back to the network" offline strategy:
-	 * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+	 * "Cache then network" offline strategy:
+	 * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
 	 */
-	/*e.respondWith(
-	  caches.match(e.request).then(function(response) {
-		return response || fetch(e.request);
-	  })
-	); */
-	
 	e.respondWith(
 		caches.open(cacheName).then(function(cache) {
 		  return fetch(e.request).then(function(response) {
